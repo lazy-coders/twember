561b39dedcf3ecd04a7f3908a867749b7c87d50e
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-03-14 01:22:57 +0100

    Basic REST actions coded, refactor needed

--- grails-app/controllers/es/ctrlz/twember/MessageController.groovy
+++ grails-app/controllers/es/ctrlz/twember/MessageController.groovy
@@ -6,15 +6,79 @@ import grails.plugins.springsecurity.Secured
 @Secured(["isAuthenticated()"])
 class MessageController {
 
+	def springSecurityService
+
     def index() {
 		render Message.findAll() as JSON
 	}
 
-	def show() {}
+	def show() {
+		def message = Message.get(params.id)
+		if (message) {
+			render message as JSON
+		} else {
+			def result = [result: 'Message not found']
+			render result as JSON
+		}
+	}
 
-	def update() {}
+	def update(MessageCommand cmd) {
+		if (cmd.validate()) {
+			def message = Message.get(params.id)
+			if (message) {
+				if (message.user.id == springSecurityService.currentUser.id) {
+					message.body = cmd.body
+					message.save(failOnError: true)
+					render message as JSON
+				} else {
+					def result = [result: 'You are not the owner of this message']
+					render result as JSON
+				}
+			} else {
+				def result = [result: 'Message not found']
+				render result as JSON
+			}
+		} else {
+			def result = [result: 'Invalid message']
+			render result as JSON
+		}
+	}
 
-	def save() {}
+	def save(MessageCommand cmd) {
+		if (cmd.validate()) {
+			def message = new Message(body: cmd.body)
+			springSecurityService.currentUser.addToMessages(message).save(failOnError: true)
+			render message as JSON
+		} else {
+			def result = [result: 'Invalid message']
+			render result as JSON
+		}
+	}
 
-	def delete() {}
+	def delete() {
+		def message = Message.get(params.id)
+		if (message) {
+			if (message.user.id == springSecurityService.currentUser.id) {
+				message.delete()
+				def result = [result: 'Message correctly deleted']
+				render result as JSON
+			} else {
+				def result = [result: 'You are not the owner of this message']
+				render result as JSON
+			}
+		} else {
+			def result = [result: 'Message not found']
+			render result as JSON
+		}
+	}
+
+
+	class MessageCommand {
+		String body
+
+		static constraints = {
+			body blank: false
+		}
+	}
+
 }
