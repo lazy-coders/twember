45ce22809c3d1cd0a210608275c766002f397a76
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-03-14 01:56:06 +0100

    Ember resources declared

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,10 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+	ember {
+		dependsOn 'jquery'
+		resource url:'js/handlebars/handlebars.js'
+		resource url:'js/emberjs/ember-1.0.0-rc.1.js'
+	}
 }
